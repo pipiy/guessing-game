6e9e972c2285829a05822579ae4fce7ebffaa180
Artsiom <artem.slabodnik@gmail.com>
2017-02-05 19:00:19 +0300

    implemented javascript file

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,31 @@
 class GuessingGame {
+    // 1. 0 - 4048
+    // 2. 0 - 2024 - 4048
+    // 3. 0 - 1012 - 2024
+    // 4. 0 - 506 - 1012
+    // 5. 0 - 253 - 506
+    // 6. 253 - 506 ( (0 - 506-253) / 2 )
+
     constructor() {}
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
-    guess() {
+    // [this.min..this.max]
 
+    guess() {
+      this.g = this.min + Math.ceil((this.max - this.min) / 2);
+      return this.g;
     }
 
     lower() {
-
+      this.max = this.g;
     }
 
     greater() {
-
+      this.min = this.g;
     }
 }
 
